72a2c4f478b59993c3b39643150df951532e137e
erichstuder <richu_@hotmail.com>
2019-01-20 19:52:00 +0100

    spi receive not yet working

--- source/arduino/arduino.ino
+++ source/arduino/arduino.ino
@@ -1,12 +1,58 @@
+#include <SPI.h> //probly not needed
+
 #define DATA_READY_PIN A0
 
+
 void setup() {
-  pinMode(DATA_READY_PIN, OUTPUT);
+	delay(2000);
+	pinMode(DATA_READY_PIN, OUTPUT);
+	pinMode(LED_BUILTIN, OUTPUT); //debug
+
+	// Enable SPI as slave.
+	pinMode(SCK, INPUT);
+	pinMode(MOSI, INPUT);
+	pinMode(MISO, OUTPUT); //always an output as only one slave exists
+	pinMode(SS, INPUT); //not used as only one slave exists
+
+	SPDR = 0xAA; //debug
+	SPCR |= (1 << SPIE); //enable interrupt
+	SPCR |= (1 << SPE); //enable
 }
 
+//debug
+//void loop() {
+//	digitalWrite(LED_BUILTIN, digitalRead(SCK));	
+//}
+
 void loop() {
-  digitalWrite(DATA_READY_PIN, HIGH);
-  delay(100);
-  digitalWrite(DATA_READY_PIN, LOW);
-  delay(900);
+	int cnt = 0;
+	SPDR = 0xAA;
+	delay(100);
+	
+
+	if(SPSR & (1 << SPIF)){
+		digitalWrite(LED_BUILTIN, HIGH);
+		//if(digitalRead(LED_BUILTIN)){
+			//digitalWrite(LED_BUILTIN, LOW);
+		//}else{
+			//digitalWrite(LED_BUILTIN, HIGH);
+		//}
+	}
+
+	digitalWrite(DATA_READY_PIN, LOW);
+	delay(900);
+}
+
+// interrupt vector definitions for leonardo:
+// C:\Program Files (x86)\Arduino\hardware\tools\avr\avr\include\avr\iom32u4.h
+ISR(SPI_STC_vect){
+	static byte nextValueToSend;
+	SPDR = nextValueToSend;
+	nextValueToSend++;
+//	if(digitalRead(LED_BUILTIN)){
+//		digitalWrite(LED_BUILTIN, LOW);
+//	}else{
+//		digitalWrite(LED_BUILTIN, HIGH);
+//	}
+	
 }
