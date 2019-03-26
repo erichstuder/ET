895b5b4c69a4c1e2e74c988aabe1eeec83beb39e
erichstuder <richu_@hotmail.com>
2019-03-26 15:28:51 +0100

    made server example run

--- source/client/tryouts/networkServer.c
+++ source/client/tryouts/networkServer.c
@@ -3,71 +3,71 @@
 #include <netinet/in.h>
 #include <sys/un.h>
 
-#define BUFFER 2
+#define BUFFER 1024
 
 int main(void)
 {
-    printf("Configuring environment... ");
+	int port = 65432;
+	char data[BUFFER];
+	struct sockaddr_in dir;
+	struct sockaddr client;
+	socklen_t long_client;
+	int id, idReuse=1, son, aux;
+	
+	memset(&dir,0,sizeof(dir));
+	dir.sin_port = htons(port);
+	dir.sin_family = AF_INET;
+	dir.sin_addr.s_addr = INADDR_ANY;
 
-    int p = 3333; // port
-    char data[BUFFER];
-    struct sockaddr_in dir;
-    struct sockaddr client;
-    socklen_t long_client;
-    int id, idReuse=1, son, aux;
+	printf("Creating socket... ");
+	id = socket(AF_INET, SOCK_STREAM, 0);
+	if (id == -1)
+		return -1;
+	printf("done!\n");
 
-    memset(&dir,0,sizeof(dir));
-    dir.sin_port = p;
-    dir.sin_family = AF_INET;
-    dir.sin_addr.s_addr = INADDR_ANY;
-    printf("done!\n");
+	printf("Configuring socket... ");
+	if(setsockopt(id,SOL_SOCKET,SO_REUSEADDR,&idReuse,sizeof(idReuse))==-1)
+	   return -1;
+	printf("done!\n");
 
-    printf("Creating socket... ");
-    id = socket(AF_INET, SOCK_STREAM, 0);
-    if (id == -1)
-        return -1;
-    printf("done!\n");
+	printf("Binding... ");
+	if(bind(id, (struct sockaddr *)&dir, sizeof(dir)) == -1)
+	{
+		close (id);
+		return -1;
+	}
+	printf("done!\n");
 
-    printf("Configuring socket... ");
-    if(setsockopt(id,SOL_SOCKET,SO_REUSEADDR,&idReuse,sizeof(idReuse))==-1)
-       return -1;
-    printf("done!\n");
+	printf("Listening... ");
+	if (listen(id , 1) == -1)
+	{
+		close(id);
+		return -1;
+	}
+	printf("done!\n");
 
-    printf("Binding... ");
-    if(bind(id, (struct sockaddr *)&dir, sizeof(dir)) == -1)
-    {
-        close (id);
-        return -1;
-    }
-    printf("done!\n");
+	printf("Accepting1... \n");
+	long_client = sizeof (client);
+	printf("Accepting2... \n");
+	printf("%d \n",id);
+	son = accept(id, &client, &long_client);
+	printf("Accepting3... \n");
+	if (son == -1)
+		return -1;
+	printf("done!\n");
 
-    printf("Listening... ");
-    if (listen(id , 1) == -1)
-    {
-        close(id);
-        return -1;
-    }
-    printf("done!\n");
+	printf("Reading... ");
+	aux = read(son, data , 1);
+	if(aux!=1)
+		return -1;
+	printf("\"%c\" ", data[0]);
+	printf("done!\n");
 
-    printf("Accepting... ");
-    long_client = sizeof (client);
-    son = accept(id, &client, &long_client);
-    if (son == -1)
-        return -1;
-    printf("done!\n");
+	printf("Writing \"S\"... ");
+	aux = send(son, "S", 1, MSG_NOSIGNAL);
+	if(aux < 0)
+		return -1;
+	printf("done!\n");
 
-    printf("Reading... ");
-    aux = read(son, data , 1);
-    if(aux!=1)
-        return -1;
-    printf("\"%c\" ", data[0]);
-    printf("done!\n");
-
-    printf("Writing \"S\"... ");
-    aux = send(son, "S", 1, MSG_NOSIGNAL);
-    if(aux < 0)
-        return -1;
-    printf("done!\n");
-
-    return 0;
+	return 0;
 }
